dfe35119a05ce6980def088945f00c0dc3150240
nicole pizzano <pizzano.nicole@gmail.com>
2022-12-19 12:54:40 -0500

    about.js

--- pages/page/about.js
+++ pages/page/about.js
@@ -23,7 +23,7 @@ href="/"> Home </Link>
 <p className={styles.text} > Well, lets see... I started off as a hobby photographer/ artist, and during this time I became familiar with the Adobe creative suite + Graphic Art. 
 However, I found myself wanting to do more. 
 ( Like creating entire web-based experiences for businesses. )
-While personal branding is something I am familiar with ( I own a small online vintage resale business ), 
+While personal branding is something I am familiar with ( I own a small online vintage resale business + an online art store ), 
 my most intense passion is for designing websites and apps. This is where I feel the most powerful creativity. I like the idea of making the web a beautful and functional place,
 and tying the business identity into personalized web experiences. Plus, it makes me feel smart to be able to do this.</p>
        <p className={styles.text}> I am currently working on a full stack CRUD app, and am looking for a job in the field. 
